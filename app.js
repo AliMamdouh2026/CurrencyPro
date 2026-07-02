7696ec736bea8349c7aff6e2cd9ce903720c97cc
AliMamdouh2026 <lwrdly779@gmail.com>
2026-07-02 17:08:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -79,3 +79,15 @@ toSelect.addEventListener("change", convert);
 
 // البداية
 loadCurrencies();
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js")
+      .then(() => {
+        console.log("Service Worker Registered");
+      })
+      .catch(err => {
+        console.log("Service Worker Error:", err);
+      });
+  });
+}
